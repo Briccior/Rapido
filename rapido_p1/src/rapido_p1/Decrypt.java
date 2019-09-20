8a08fc20eb179e861e73a4f776e927fc81c03e94
Ricky <briccior@knights.ucf.edu>
2019-09-20 11:17:58 -0400

    Finished Encrypt Decrypt files

--- rapido_p1/src/rapido_p1/Decrypt.java
+++ rapido_p1/src/rapido_p1/Decrypt.java
@@ -1,5 +1,36 @@
 package rapido_p1;
 
+import java.util.Scanner;
+
 public class Decrypt {
+	
+	public static void main(String[] args) {
+		
+		Scanner scnr = new Scanner(System.in);
+		
+		int x;
+		int ones;
+		int tens;
+		int hundreds;
+		int thousands;
+		int tmpVal;
+		
+		System.out.println("Enter a number to decrypt: ");
+		x = scnr.nextInt();
+		
+		ones = ((x % 10) + 3) % 10;
+		tmpVal = x / 10;
+		tens = ((tmpVal % 10) + 3) % 10;
+		tmpVal = tmpVal / 10;
+		hundreds = ((tmpVal % 10) + 3) % 10;
+		tmpVal = tmpVal / 10;
+		thousands = ((tmpVal % 10) + 3) % 10;
+		
+		System.out.println(thousands + " " + hundreds + " " + tens + " " + ones);
+		
+		x = (tens * 1000) + (ones * 100) + (thousands * 10) + (hundreds);
+		
+		System.out.println("The Decrypted number is:" + x);
+	}
 
 }
--- rapido_p1/src/rapido_p1/Encrypt.java
+++ rapido_p1/src/rapido_p1/Encrypt.java
@@ -1,10 +1,37 @@
 package rapido_p1;
 
+import java.util.Scanner;
+
 public class Encrypt {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scnr = new Scanner(System.in);
+		
+		int x;
+		int ones;
+		int tens;
+		int hundreds;
+		int thousands;
+		int tmpVal;
+		
+		System.out.println("Enter a number to encrypt: ");
+		x = scnr.nextInt();
+		
+		ones = ((x % 10) + 7) % 10;
+		tmpVal = x / 10;
+		tens = ((tmpVal % 10) + 7) % 10;
+		tmpVal = tmpVal / 10;
+		hundreds = ((tmpVal % 10) +7) % 10;
+		tmpVal = tmpVal / 10;
+		thousands = ((tmpVal % 10) + 7) % 10;
+		
+		//System.out.println(thousands + " " + hundreds + " " + tens + " " + ones);
+		
+		x = (tens * 1000) + (ones * 100) + (thousands * 10) + (hundreds);
+		
+		System.out.println("The Encrypted number is:" + x);
+		
 	}
 
 }
